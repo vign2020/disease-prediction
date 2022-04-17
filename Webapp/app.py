a7487be2843f8e884353725f006de66853a9b8bc
Jay8181 <kumarjai2466@gmail.com>
2022-04-17 16:03:10 +0530

    Added Files For Webapp

---
+++ Webapp/app.py
@@ -0,0 +1,48 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template, redirect
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+
+@app.route('/kdp')
+def kdp():
+    return render_template('kdp.html')
+
+
+@app.route('/kdp',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float((x)) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    output = round(prediction[0], 2)
+    if output == 1:
+        s = 'Positive Results(Chronic Kidney Disease)'
+    else:
+       s = 'Negative Results(No Chronic Kidney Disease)'
+    return render_template('kdp.html', prediction_text='{}'.format(s))
+
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
